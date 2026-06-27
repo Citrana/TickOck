8678147b93c55d11885c9d75220d1211a114504d
barakadanny <barakadan421@gmail.com>
2026-06-27 19:25:54 +0300

    Fix footer to always stick to the bottom of the page

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -12,6 +12,7 @@ import {AuthGuard} from '@/components/auth/AuthGuard';
 import {routing} from '@/i18n/routing';
 import {Toaster} from 'sonner';
 import {ServiceWorkerRegistration} from '@/components/pwa/ServiceWorkerRegistration';
+import {TopProgressBar} from '@/components/ui/TopProgressBar';
 
 const geistSans = localFont({
   src: '../fonts/GeistVF.woff',
@@ -66,17 +67,19 @@ export default async function LocaleLayout({
     <ConvexAuthNextjsServerProvider>
       <html lang={locale}>
         <body
-          className={`${geistSans.variable} ${geistMono.variable} min-h-screen overflow-x-hidden bg-[#f9f7f4] antialiased`}
+          className={`${geistSans.variable} ${geistMono.variable} flex min-h-screen flex-col overflow-x-hidden bg-[#f9f7f4] antialiased`}
         >
           <NextIntlClientProvider messages={messages}>
             <ConvexClientProvider>
-              {/* Side-effect guard: signs out suspended/banned users */}
-              <AuthGuard />
-              <Navbar />
-              <main className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
-                {children}
-              </main>
-              <Toaster position="top-right" richColors />
+              <TopProgressBar>
+                {/* Side-effect guard: signs out suspended/banned users */}
+                <AuthGuard />
+                <Navbar />
+                <main className="mx-auto w-full max-w-7xl flex-1 px-4 py-8 sm:px-6 lg:px-8">
+                  {children}
+                </main>
+                <Toaster position="top-right" richColors />
+              </TopProgressBar>
             </ConvexClientProvider>
           </NextIntlClientProvider>
           <Footer />
